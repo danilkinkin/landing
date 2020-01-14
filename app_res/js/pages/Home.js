e6af8d519c7d109c8eaf94dfddd6c57c9c657c37
Danilkinkin <Danilkinkin@gmail.com>
2020-01-15 00:59:10 +0400

    Add age calculation

--- app_res/js/pages/Home.js
+++ app_res/js/pages/Home.js
@@ -4,8 +4,16 @@ import { getSafeValue as LOC} from "../utils/Locale.js";
 
 import Link from "../components/custom/Link.js";
 
-function Age({now}){
-	return 20;
+function Age(now){
+	const born = new Date('1999-02-13T22:30:00+0500');
+
+	const correctiveMonth = now.getMonth() - born.getMonth() < 0;
+	const correctiveDate = now.getDate() - born.getDate() < 0;
+	const correctiveHours = now.getHours() - born.getHours() < 0;
+	const correctiveMinutes = now.getMinutes() - born.getMinutes() < 0;
+	const corrective = (correctiveMonth || correctiveDate || correctiveHours || correctiveMinutes)? -1 : 0;
+
+	return now.getFullYear() - born.getFullYear() + corrective;
 }
 
 function Home(){
@@ -16,7 +24,7 @@ function Home(){
 	UI("h1").text("Danilkinkin").insert(body);
 	UI("h3").text("web UX/UI developer").insert(body);
 	UI("p")
-		.text(LOC("about_me", Age({now: Date.now()})))
+		.text(LOC("about_me", Age(new Date())))
 		.insert(body);
 	UI()
 		.className("footer")
