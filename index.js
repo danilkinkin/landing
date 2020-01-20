efb4976ddb3eac19938d2f0acb1ea05ecda17398
Danilkinkin <Danilkinkin@gmail.com>
2020-01-21 00:50:21 +0400

    Add logs

--- index.js
+++ index.js
@@ -5,27 +5,39 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+console.log('Starting Server...');
+
 const isRelease = process.env.RELEASE === "prod";
+
+console.log('Deploy:', isRelease? 'release' : 'develop');
+
 const sslKeys = process.env.SSL_KEYS && JSON.parse(process.env.SSL_KEYS);
+
+console.log('HTTPS:', sslKeys? 'on' : 'off');
+
 const port = isRelease? (sslKeys? 443 : 80) : 3000;
+
+console.log('Port:', port);
+
 const options = sslKeys && {
     key: fs.readFileSync(sslKeys.key, 'utf8'),
     cert: fs.readFileSync(sslKeys.cert, 'utf8'),
 };
 const server = isRelease? https.createServer(options, app) : http.createServer(app);
 
-app
-	.set('view engine', 'ejs')
-	.use('/app_res', express.static(path.join(__dirname, '/app_res')))
-	.use('/.well-known', express.static(path.join(__dirname, '/.well-known')))
-	.get('/*', (req, res) => {
-		res.sendFile(path.join(__dirname, '/index.html'));
-	});
+try {
+	app
+		.set('view engine', 'ejs')
+		.use('/app_res', express.static(path.join(__dirname, '/app_res')))
+		.use('/.well-known', express.static(path.join(__dirname, '/.well-known')))
+		.get('/*', (req, res) => {
+			res.sendFile(path.join(__dirname, '/index.html'));
+		});
 
-server
-	.listen(port, () => {
-		console.log('Starting Server...');
-		console.log('HTTPS:', sslKeys? 'on' : 'off');
-		console.log('Deploy:', isRelease? 'release' : 'develop');
-		console.log('Start on port:', port);
-	});
+	server
+		.listen(port, () => {
+			console.log('Starting successfull');
+		});
+} catch(e) {
+	console.log('Error start server', e);
+}		
