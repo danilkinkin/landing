0b5924773da8acc0bf397b7b5ee7e10aae69ef5d
Danilkinkin <Danilkinkin@gmail.com>
2020-01-21 00:00:35 +0400

    Add ssl

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-var server = require('express');
-var app = server();
-var path = require('path')
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const isRelease = process.env.RELEASE;
+const sslKeys = process.env.SSL_KEYS;
+const port = isRelease? (sslKeys? 443 : 80) : 3000;
+const options = sslKeys && {
+    key: fs.readFileSync(sslKeys.key),
+    cert: fs.readFileSync(sslKeys.cert),
+};
+const server = isRelease? https.createServer(options, express()) : http.createServer(express());
 
 app
 	.set('view engine', 'ejs')
-	.use('/app_res', server.static(path.join(__dirname, '/app_res')))
-	.use('/.well-known', server.static(path.join(__dirname, '/.well-known')))
+	.use('/app_res', express.static(path.join(__dirname, '/app_res')))
+	.use('/.well-known', express.static(path.join(__dirname, '/.well-known')))
 	.get('/*', (req, res) => {
 		res.sendFile(path.join(__dirname, '/index.html'));
 	})
-	/*.get('*', function(req, res){
-		res.redirect("/");
-	})*/
-	.listen(process.env.RELEASE === "production" && 80 || 3000, () => {
-
-		console.log('Start on port:', process.env.RELEASE === "production" && 80 || 3000);
-
-		if(process.env.RELEASE === "production"){
-			console.log('Is production release');
-		}
+	.listen(isRelease && 80 || 3000, () => {
+		console.log('Starting Server...');
+		console.log('HTTPS:', sslKeys? 'on' : 'off');
+		console.log('Deploy:', isRelease? 'release' : 'develop');
+		console.log('Start on port:', port);
 	});
